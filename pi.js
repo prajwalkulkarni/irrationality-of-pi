615f276776c27dff250c6135d377a3eb18e31a54
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2023-10-28 19:24:21 +0530

    Add additional controls

--- pi.js
+++ pi.js
@@ -1,27 +1,63 @@
 const playPause = document.getElementById("play-pause");
-
-let theta = 0;
-let thetaOfSecondaryLine = 0;
-const pi = Math.PI;
-const trail = [];
-
-let play = true;
+const zoomIn = document.getElementById("plus");
+const zoomOut = document.getElementById("minus");
+const reset = document.getElementById("reset");
 
 playPause.addEventListener("click", () => {
   play = !play;
   if (play) {
     playPause.innerHTML = "Pause";
-    window.requestAnimationFrame(piBeingIrrational);
+    playPause.style.width = "60px";
+    piBeingIrrational();
   } else {
     playPause.innerHTML = "Play";
+    playPause.style.width = "50px";
   }
 });
 
+let scale = 1.2;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("click", (e) => {
+  ctx.translate(e.offsetX, e.offsetY);
+  ctx.scale(scale, scale);
+  ctx.translate(-e.offsetX, -e.offsetY);
+  if (!play) {
+    piBeingIrrational();
+  }
+});
+zoomIn.addEventListener("click", (e) => {
+  ctx.translate(150, 150);
+  ctx.scale(scale, scale);
+  ctx.translate(-150, -150);
+  if (!play) {
+    piBeingIrrational();
+  }
+});
+
+zoomOut.addEventListener("click", () => {
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  if (!play) {
+    piBeingIrrational();
+  }
+});
+
+let theta = 0.8901179185171084;
+const pi = Math.PI;
+const trail = [];
+let play = true;
+
+reset.addEventListener("click", () => {
+  theta = 0.8901179185171084;
+  trail.length = 0;
+
+  if (!play) {
+    piBeingIrrational();
+  }
+});
 function piBeingIrrational() {
-  const canvas = document.getElementById("canvas");
-
-  const ctx = canvas.getContext("2d");
-
   ctx.strokeStyle = "black";
   ctx.clearRect(0, 0, 300, 300);
   ctx.strokeRect(0, 0, 300, 300);
@@ -31,13 +67,14 @@ function piBeingIrrational() {
 
   const radius = 40;
 
-  const primaryHandEndX = centerX + radius * Math.cos(theta);
-  const primaryHandEndY = centerY + radius * Math.sin(theta);
+  const primaryHandEndX = centerX - radius * Math.cos(theta);
+  const primaryHandEndY = centerY - radius * Math.sin(theta);
 
   ctx.save();
-  ctx.lineWidth = 0.8;
+  ctx.lineWidth = 0.5;
 
   ctx.translate(centerX, centerY);
+
   ctx.beginPath();
   ctx.moveTo(0, 0);
   ctx.lineTo(primaryHandEndX - centerX, primaryHandEndY - centerY);
@@ -62,14 +99,19 @@ function piBeingIrrational() {
     y: drawingHandEndY,
   });
 
+  ctx.save();
   ctx.beginPath();
   ctx.moveTo(trail[0].x, trail[0].y);
+  ctx.lineWidth = 0.5;
   for (let i = 1; i < trail.length; i++) {
     ctx.lineTo(trail[i].x, trail[i].y);
   }
   ctx.stroke();
+  ctx.restore();
 
-  theta += (pi / 180) * 1.5;
+  theta -= (pi / 180) * 1.5;
+
+  //The angles and arithemetic operations are inverted to have an anticlockwise rotation.
 
   if (play) {
     window.requestAnimationFrame(piBeingIrrational);
